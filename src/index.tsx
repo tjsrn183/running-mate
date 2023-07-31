ff71c587c3a941cab9a926087df7c6eab3e52165
seongu <tjsrn183@naver.com>
2023-08-01 00:59:46 +0900

    리덕스 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,23 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { HelmetProvider } from 'react-helmet-async';
+import rootReducer from './modules';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
+const store = createStore(rootReducer, composeWithDevTools());
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
 );
